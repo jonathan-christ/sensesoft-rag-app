7d3a19fa4b7024c2b6ba6d5499c245b00fddf561
Matthew Cedric Calaycay <matthewc.calaycay@gmail.com>
2025-09-19 02:20:44 +0800

    feat: implement basic login form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,5 @@
-import { GalleryVerticalEnd } from "lucide-react"
-
-import { LoginForm } from "@/components/login-form"
-
+import { GalleryVerticalEnd } from "lucide-react";
+import { LoginForm } from "./components/login-form";
 export default function LoginPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
@@ -15,5 +13,5 @@ export default function LoginPage() {
         <LoginForm />
       </div>
     </div>
-  )
+  );
 }
