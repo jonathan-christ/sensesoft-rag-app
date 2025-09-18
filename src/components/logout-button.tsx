f36bae5e61e76d2ef6b406dc836211cda2f43e2d
Matthew Cedric Calaycay <matthewc.calaycay@gmail.com>
2025-09-19 02:36:00 +0800

    feat: implement auth + logout to `/` home directory

---
+++ src/components/logout-button.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { createClient } from "@/lib/supabase/client";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+export function LogoutButton() {
+  const router = useRouter();
+
+  const logout = async () => {
+    const supabase = createClient();
+    await supabase.auth.signOut();
+    router.push("/login");
+  };
+
+  return (
+    <Button
+      className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+      onClick={logout}
+    >
+      Logout
+    </Button>
+  );
+}
